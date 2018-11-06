055dcce6340c34063c25433dfdfe6e3cb598ff0d
Taras_Kovalev <taraswww777@mail.ru>
2018-11-06 17:54:40 +0300

    create base page-proofs

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ComponentBEM from './components/ComponentBEM';
+import './App.scss';
+import demoData from './demoData';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import NetflixSearchField from './components/NetflixSearchField/NetflixSearchField';
+import NetflixSearchResults from "./components/NetflixSearchResults/NetflixSearchResults";
+
+class App extends ComponentBEM {
+	componentName = 'app';
+
+	render() {
+		return (
+			<div className={this.block()}>
+				<header className={this.elem('header')}>
+					<div className={this.elem('container')}>
+						<div className={this.elem('row')}>
+							<div className={this.elem('header-title')}>netflix roulette</div>
+
+							<div className={this.elem('search-area')}>
+								<div className={this.elem('search-area-title')}>Find your movie</div>
+								<div className={this.elem('search-area-field')}>
+									<NetflixSearchField/>
+								</div>
+							</div>
+
+							<div className={this.elem('search-by')}>
+								<div className={this.elem('search-by-title')}>search by</div>
+								<div className={this.elem('search-by-selectors')}>
+									{/*<SearchBy/>*/}
+								</div>
+								<div className={this.elem('search-by-run')}>
+									{/*<SearchButton/>*/}
+								</div>
+							</div>
+						</div>
+					</div>
+				</header>
+				<main className={this.elem('main')}>
+					<div className={this.elem('container')}>
+						<div className={this.elem('row')}>
+							<NetflixSearchResults listFilms={demoData.listFilms}/>
+						</div>
+					</div>
+				</main>
+
+				<footer className={this.elem('footer')}>
+					<div className={this.elem('container')}>
+						<div className={this.elem('row')}>
+							<div className={this.elem('footer-title')}>netflix roulette</div>
+						</div>
+					</div>
+				</footer>
+			</div>
+		);
+	}
 }
 
 export default App;
