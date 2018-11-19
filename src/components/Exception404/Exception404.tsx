6505a69b0958721c108c4d5ba0048b2361516db8
Taras_Kovalev <taraswww777@mail.ru>
2018-11-19 13:07:15 +0300

    component Exception404 migrate to typescript

--- src/components/Exception404/Exception404.tsx
+++ src/components/Exception404/Exception404.tsx
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 
-
 class Exception404 extends Component {
-	componentName = 'exception-404';
 
 	render() {
-	  return (
-			<div className={this.block()}>
+		return (
+			<div>
 				<h1>404 page note found</h1>
 			</div>
-	  );
+		);
 	}
 }
 
